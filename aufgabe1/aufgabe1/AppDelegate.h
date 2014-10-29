d0daefbe07528440b93638d5a2bc7ad87ee150bd
Felix Jensen <felixjensn@gmail.com>
2014-10-29 12:41:38 +0100

    initial project structure

---
+++ aufgabe1/aufgabe1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aufgabe1
+//
+//  Created by Felix Jensen on 29.10.14.
+//  Copyright (c) 2014 Felix Jensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aufgabe1/aufgabe1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  aufgabe1
+//
+//  Created by Felix Jensen on 29.10.14.
+//  Copyright (c) 2014 Felix Jensen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
