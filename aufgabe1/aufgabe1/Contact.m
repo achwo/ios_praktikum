f1bd417b6cbb1437d961e5ae8aacc5b1240733ad
Felix Jensen <felixjensn@gmail.com>
2014-10-29 13:10:19 +0100

    added Contact

---
+++ aufgabe1/aufgabe1/Contact.m
@@ -0,0 +1,23 @@
+//
+//  Contact.m
+//  aufgabe1
+//
+//  Created by Felix Jensen on 29.10.14.
+//  Copyright (c) 2014 Felix Jensen. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+-(id)init
+{
+    if (self = [super init]) {
+        _firstname = @"Vorname";
+        _lastname = @"Nachname";
+        _email = @"vorname@nachname";
+    }
+    return self;
+}
+
+@end
