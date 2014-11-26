baf50585450ee9f84fc326f5d8c63d89fd950201
Felix Jensen <felixjensn@gmail.com>
2014-11-26 14:14:27 +0100

    initial new project state

---
+++ ios_praktikum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_praktikum
+//
+//  Created by Felix Jensen on 26.11.14.
+//  Copyright (c) 2014 Felix Jensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios_praktikum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ios_praktikum
+//
+//  Created by Felix Jensen on 26.11.14.
+//  Copyright (c) 2014 Felix Jensen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
